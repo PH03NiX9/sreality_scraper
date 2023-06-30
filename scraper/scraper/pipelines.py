a90992f91eb500b2853581e006b817a9ce5624ed
PH03NiX9 <zdenekpetracek@email.cz>
2023-06-30 16:25:25 +0200

    Added a pipeline for the PostgreSQL (to be done)

--- scraper/scraper/pipelines.py
+++ scraper/scraper/pipelines.py
@@ -1,13 +1,29 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
+import psycopg2
+import logging
 from itemadapter import ItemAdapter
 
-
 class ScraperPipeline:
+    def __init__(self):
+        self.create_connection()
+
+    def create_connection(self):
+        pass
+
+    def insertData(self, item):
+        try:
+            adapter = ItemAdapter(item)
+            title = adapter.get('title')
+            image_urls = adapter.get('image_urls')
+
+            for image_url in image_urls:
+                # Execute an INSERT query to insert the data into the table
+                self.curr.execute("INSERT INTO scraped (title, image_url) VALUES (%s, %s)", (title, image_url))
+
+                # Commit the transaction to persist the changes
+                self.connection.commit()
+        except Exception as e:
+            logging.error("Error inserting data: %s", str(e))
+
     def process_item(self, item, spider):
+        self.insertData(item)
         return item
