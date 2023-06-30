5e45c9e02fe614e28fd09f74e7aa260406dda496
PH03NiX9 <zdenekpetracek@email.cz>
2023-06-30 15:49:44 +0200

    Adding a spider

--- scraper/scraper/spiders/srealityspider.py
+++ scraper/scraper/spiders/srealityspider.py
@@ -1,10 +1,30 @@
 import scrapy
+import json
 
 
-class SrealityspiderSpider(scrapy.Spider):
+class SrealitySpider(scrapy.Spider):
     name = "srealityspider"
     allowed_domains = ["sreality.cz"]
-    start_urls = ["https://sreality.cz"]
+
+    def start_requests(self):
+        base_url = "https://www.sreality.cz/api/cs/v2/estates?category_main_cb=1&category_type_cb=1&page="
+        total_listings = 0
+        page = 1
+
+        while total_listings < 500 and page <= 20:
+            url = base_url + str(page)
+            yield scrapy.Request(url, callback=self.parse)
+            page += 1
 
     def parse(self, response):
-        pass
+        data = json.loads(response.body)
+        items = data["_embedded"]["estates"]
+        for item in items[:500]:
+            title = item["name"]
+            images = item.get("_links", {}).get("images", [])
+            image_urls = [image["href"] for image in images]
+
+            yield {
+                'title': title,
+                'image_urls': image_urls
+            }
